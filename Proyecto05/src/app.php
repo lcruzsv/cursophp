ff95e64136f70d1e765d7146d9fbbb396624ca55
Luis Alberto Cruz Orellana <lcruz@grupoq.com>
2017-02-03 14:21:13 -0600

    Login

--- Proyecto05/src/app.php
+++ Proyecto05/src/app.php
@@ -10,6 +10,9 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\User;
 
+//Multiples url en firewall
+use Symfony\Component\HttpFoundation\RequestMatcher;
+
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
@@ -34,9 +37,13 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 require_once 'schema.php'; #Todo esto no me gusta
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
-        'admin' => array(
-            'pattern' => '^/admin',
-            'http' => true,
+      'publico' => array(
+          'pattern' =>  '^/usuario',          
+          ),
+
+        'privado' => array(
+            'pattern' =>  '^/',
+            'form' => array('login_path' => 'login', 'check_path' => '/admin/login_check'),
             'users' => function () use ($app) {
                   return new models\UserProvider($app['db']);
               },
@@ -46,7 +53,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 ));
 
 //Montar controles
-$app->mount('/tareas', include 'controllers/tareas.php');
+$app->mount('/', include 'controllers/tareas.php');
 $app->mount('/usuario', include 'controllers/usuarios.php');
 
 return $app;
--- Proyecto05/src/controllers/usuarios.php
+++ Proyecto05/src/controllers/usuarios.php
@@ -1,5 +1,10 @@
 <?php
 $usuarios = $app['controllers_factory'];
+
+$usuarios->get('/login', function () use($app){
+
+  return $app['twig']->render('usuarios/login.html.twig');
+})->bind('login');
 $usuarios->get('/', function () { return 'Blog home page'; });
 $usuarios->get('/resetear', function () { return 'Ingresa tu correo'; });
 
